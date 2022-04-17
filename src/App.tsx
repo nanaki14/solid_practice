383814b2cbb3a6763674d13a76e963f9a20f14e0
nanaki14 <masatochiba0122@gmail.com>
2022-04-17 17:47:37 +0900

    router

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,18 @@ import { Component, onMount } from 'solid-js';
 import { CounterProvider } from './components/Counter';
 import { DynamicComponent } from './components/DynamicComponent';
 import { Heading } from './components/Heading'
+import { Routes, Route, Link, useParams, Outlet } from "solid-app-router"
+
+const UserDetail = () => {
+  const params = useParams();
+
+  return (
+    <div>
+      <h1>User: {params.id}</h1>
+      <Link href="/">Home</Link>
+    </div>
+  )
+}
 
 const App: Component = () => {
   let myDiv: any;
@@ -25,6 +37,34 @@ const App: Component = () => {
           text="これはいいものだ"
         />
         <DynamicComponent />
+        <Routes>
+          <Route path="/" element={
+            <div>
+              <h1>Home</h1>
+              <Link href="/about">about</Link>
+              <br />
+              <Link href="/users/01">user</Link>
+            </div>
+          } />
+          <Route path="/about" element={
+            <div>
+              <h1>About</h1>
+              <Link href="/">home</Link>
+            </div>
+          }/>
+          <Route path="/users"
+            element={
+              <div>
+                <h1>Users</h1>
+                <Outlet />
+              </div>
+            }
+          >
+            <Route path="/:id" element={
+              <UserDetail />
+            }/>
+          </Route>
+        </Routes>
       </div>
     </CounterProvider>
   );
